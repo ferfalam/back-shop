c3afd9bb97973916d0c51330fabc0437e41502ff
Farid AMOUSSA <amoussa.farid2@gmail.com>
2022-01-14 07:41:24 +0100

    comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
   {
     Paginator::useBootstrap();
     Schema::defaultStringLength(191);
+    if(config('app.env') === 'production') {
+        \URL::forceScheme('https');
+    }
   }
 
   /**
